38ed064e108f42296603439aa8567a2698663cdf
Евгений Цуран <tsurik_01@mail.ru>
2022-09-04 23:05:54 +0300

    2

--- example_for_log.py
+++ example_for_log.py
@@ -5,15 +5,15 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR = os.path.join(BASE_DIR, 'main.log')
 
-logger = logging.getLogger(__name__)
 
-def loggin():
+def set_loggin():
     logging.basicConfig(
         level=logging.DEBUG,
         format=(
-            '%(asctime)s::[%(levelname)s]::%(message)s, %(name)s, %(lineno)s, %(filename)s'),
+            '%(asctime)s::[%(levelname)s]::%(message)s, '
+            '%(name)s, %(lineno)s, %(filename)s'
+        ),
         filename=MAIN_DIR,
         handlers=[logging.StreamHandler(sys.stdout)],
         filemode='w'
     )
-
--- homework.py
+++ homework.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import time
-
 from http import HTTPStatus
 from json import JSONDecodeError
 
@@ -11,26 +10,29 @@ from dotenv import load_dotenv
 from requests.exceptions import RequestException
 from telegram import TelegramError
 
-from example_for_log import logger
+from example_for_log import set_loggin
 from exceptions import (
-    CustomJSONDecodeError, CustomRequestException,
-    CustomTelegramError, HomeworkIsNotList, ListIsNotEmpty,
-    NotSentException, TheServerDidNotSendTheTimeCutoff,
+    CustomJSONDecodeError,
+    CustomRequestException,
+    CustomTelegramError,
+    HomeworkIsNotList,
+    ListIsNotEmpty,
+    NotSentException,
+    TheServerDidNotSendTheTimeCutoff,
     WrongAPIResponseCodeError
 )
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_HOME_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
-
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICT = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -39,19 +41,18 @@ HOMEWORK_VERDICT = {
 }
 
 
-def send_message(bot, message: str) -> str:
+def send_message(bot: "telegram.bot.Bot", message: str) -> None:
     """Отправляем сообщение в телеграм."""
-    chat_id = TELEGRAM_CHAT_ID
+    logger.info(message)
     try:
-        bot.send_message(chat_id, text=message)
-        logging.info(message)
+        bot.send_message(TELEGRAM_CHAT_ID, text=message)
     except TelegramError as error:
         raise CustomTelegramError('ошибка отправки сообщения') from error
 
 
 def get_api_answer(current_timestamp: int) -> dict:
     """Делает запрос к единственному эндпоинту API-сервиса."""
-    logging.info("Делаем запрос к единственному эндпоинту API-сервиса.")
+    logger.info("Делаем запрос к единственному эндпоинту API-сервиса.")
     params = {'from_date': current_timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
@@ -72,9 +73,9 @@ def get_api_answer(current_timestamp: int) -> dict:
         raise CustomJSONDecodeError('некорректный ответ сервера') from exc
 
 
-def check_response(response) -> dict:
+def check_response(response: dict) -> dict:
     """Проверяем ответ API на корректность."""
-    logging.info("Проверяем ответ ответ API на корректность.")
+    logger.info("Проверяем ответ ответ API на корректность.")
     if not isinstance(response, dict):
         raise TypeError(response)
     if 'homeworks' not in response:
@@ -84,27 +85,28 @@ def check_response(response) -> dict:
         raise HomeworkIsNotList
     if not list_homework:
         raise ListIsNotEmpty
+    if 'current_date' not in response:
+        raise TheServerDidNotSendTheTimeCutoff(
+            'сервер не отправил отсечку времени'
+        )
     return list_homework[0]
 
 
 def parse_status(homework: dict) -> str:
     """Возвращаем вердикт из словаря статусов."""
-    try:
-        if 'homework_name' not in homework:
-            raise KeyError
-        homework_name = homework['homework_name']
-        homework_status = homework['status']
-        logging.info("Возвращаем вердикт из словаря статусов.")
-    except TypeError as error:
-        raise TypeError(f'Возникла ошибка {error} при запросе.') from error
+    if 'homework_name' not in homework:
+        raise KeyError
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    logger.info("Возвращаем вердикт из словаря статусов.")
     verdict = HOMEWORK_VERDICT[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens() -> bool:
     """Проверка наличия токенов."""
-    logging.info("Проверка наличия токенов.")
-    return all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
+    logger.info("Проверка наличия токенов.")
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def main():
@@ -112,23 +114,18 @@ def main():
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = 0
     current_status = str()
-    if check_tokens():
-        logger.critical('отановка программы')
+    if not check_tokens():
+        logger.critical('отановка программы, отсутствуют токены')
         exit()
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
             message = parse_status(homework)
-            if 'current_date' in response:
-                current_timestamp = response['current_date']
-                raise TheServerDidNotSendTheTimeCutoff(
-                    'сервер не отправил отсечку времени'
-                )
+            current_timestamp = response['current_date']
             if message != current_status:
                 current_status = message
                 send_message(bot, current_status)
-                logging.info(message)
             else:
                 logger.INFO('статус не изменился')
         except NotSentException as exc:
@@ -137,10 +134,8 @@ def main():
             logger.error(f'Сбой в работе программы: {error}')
             try:
                 send_message(bot, message)
-                logging.info(message)
             except TelegramError as error:
-                raise CustomTelegramError(
-                    'ошибка отправки сообщения') from error
+                logger.error(f'ошибка отправки сообщения{error}')
         finally:
             time.sleep(RETRY_TIME)
 
